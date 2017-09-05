5f68ea7bf363005d3957f2cf761b96aeea0d06ec
PJ Xiao <pjxiao@pjxiao-mn1.linkedin.biz>
2017-09-04 23:20:27 -0700

    initial setup

--- leetcode-java/src/main/java/pj/binaryTree/BT_traverse_II.java
+++ leetcode-java/src/main/java/pj/binaryTree/BT_traverse_II.java
@@ -1,4 +1,4 @@
-package BinaryTreeLevelOrderTraversal_II;
+package pj.binaryTree;
 import java.util.*;
 
 class TreeNode{
@@ -7,7 +7,7 @@ class TreeNode{
 	TreeNode right;
 	TreeNode(int x){
 		this.val = x;
-	}	
+	}
 }
 
 // this version is the fastest
@@ -15,46 +15,46 @@ public class BT_traverse_II {
 	public ArrayList<ArrayList<Integer>> levelOrderBottom(TreeNode root){
 		// 'result' will hold the node values in reverse order
 		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>> ();
-		
+
 		if(root == null){return result;}
-		
+
 		Queue<TreeNode> queue = new LinkedList<TreeNode>();
-		
+
 		// list of list 'list' to hold the node values in order
 		ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
-		
+
 		// initialize the 'queue' by adding the root node
 		queue.add(root);
-		
+
 		// process the binary tree layer-by-layer and store the node values in 'list'
 		while (!queue.isEmpty()){
 			ArrayList<TreeNode> tempNodes = new ArrayList<TreeNode>();
 			ArrayList<Integer> tempValues = new ArrayList<Integer>();
-			
+
 			// remove the same-layer nodes from the current 'queue' and store the value of each node into the 'tempValues'
 			while(!queue.isEmpty()){
 				TreeNode node = queue.remove();
 				tempNodes.add(node);
-				tempValues.add(node.val);				
+				tempValues.add(node.val);
 			}
 			list.add(tempValues);
-			
+
 			// refill the 'queue' with next level/layer nodes
 			for (TreeNode node : tempNodes){
 				if(node.left != null){
 					queue.add(node.left);
 				}
 				if(node.right != null){
-					queue.add(node.right);					
-				}				
+					queue.add(node.right);
+				}
 			}
-				
+
 		}
-		
+
 		// reverse the 'list' and store into 'result'
 		for(int i=list.size()-1; i>=0; i--){
-			result.add(list.get(i));			
-		}		
+			result.add(list.get(i));
+		}
 		return result;
-	}	
+	}
 }
